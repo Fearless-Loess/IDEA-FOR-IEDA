40e263b0cfd39e1db20ce3fef3513c573dadc3b2
Fearless-Loess <36927914+Fearless-Loess@users.noreply.github.com>
2018-02-28 10:19:52 -0600

    Update Read XL take 1.R

--- Read XL take 1.R
+++ Read XL take 1.R
@@ -1,6 +1,6 @@
 install.packages("tidyverse")
 library(tinyverse)
-#insert file name here 
-new_file <- read_excel("2017 Service Enterprise Initiative- Post Survey Fall 2017_2016 cohort only.xlsx")
-gather(new_file)
-glimpse(new_file)
+#insert file name here %>%
+read_excel("") %>%
+gather() %>%
+glimpse()
